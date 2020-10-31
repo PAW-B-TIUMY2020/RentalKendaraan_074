3ae7d81d620737410ee977c06b3e081a87682e80
eeRha <arrasyidtz3@gmail.com>
2020-10-31 12:15:15 +0700

    Dropdown done

--- RentalKendaraan_074/Models/Peminjaman.cs
+++ RentalKendaraan_074/Models/Peminjaman.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace RentalKendaraan_074.Models
 {
@@ -9,16 +10,22 @@ namespace RentalKendaraan_074.Models
         {
             Pengembalian = new HashSet<Pengembalian>();
         }
-
+        [DisplayName("ID")]
         public int IdPeminjaman { get; set; }
         public DateTime? TglPeminjaman { get; set; }
+        [DisplayName("Kendaraan")]
         public int? IdKendaraan { get; set; }
+        [DisplayName("Costumer")]
         public int? IdCostumer { get; set; }
+        [DisplayName("Jaminan")]
         public int? IdJaminan { get; set; }
         public int? Biaya { get; set; }
 
+        [DisplayName("Customer")]
         public Customer IdCostumerNavigation { get; set; }
+        [DisplayName("Jaminan")]
         public Jaminan IdJaminanNavigation { get; set; }
+        [DisplayName("Kendaraan")]
         public Kendaraan IdKendaraanNavigation { get; set; }
         public ICollection<Pengembalian> Pengembalian { get; set; }
     }
